4f9e0f9c3c618276b8e80efdf4d82ab1f2ff57a9
Carson Rey <c.rey494@gmail.com>
2018-08-03 21:14:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ function sayHiToGrandma(string){
   console.log(string.toLowerCase())
   return 'I can\'t hear you!'
   
+  console.log(string.toLowerCase())
+  return 'I can\'t hear you!'
  
   
  }
