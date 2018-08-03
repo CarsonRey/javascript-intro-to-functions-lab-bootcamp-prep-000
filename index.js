b14dfab512877bc5f2821dc6a870c23cc5f5ff38
Carson Rey <c.rey494@gmail.com>
2018-08-03 21:19:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function logWhisper(HELLO){
 
 function sayHiToGrandma(string){
   
-  if('hello'.toLowerCase())
+  if(string.toLowerCase())
   return 'I can\'t hear you!'
   
  else ('hello'.toUpperCase())
