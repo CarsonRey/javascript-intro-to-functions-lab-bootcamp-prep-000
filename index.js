ba37936f2a6b8a2747fcb6f79c56a754117d8c15
Carson Rey <c.rey494@gmail.com>
2018-08-03 21:24:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-function logShout(hello){
-  console.log(hello.toUpperCase())
+function logShout(string){
+  console.log(string.toUpperCase())
   
 }
 
-function logWhisper(HELLO){
-  console.log(HELLO.toLowerCase())
+function logWhisper(string){
+  console.log(string.toLowerCase())
 }
 
 
