ed155ede08f7186d9fbc4f10a8ac6fe5b654f122
Carson Rey <c.rey494@gmail.com>
2018-08-03 20:16:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(hello){
+  console.log(hello.toUpperCase())
+  
+}
+
+function logWhisper(HELLO){
+  console.log(HELLO.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  
+  'hello'.toLowerCase() === 'hello' 
+  return 'I can\'t hear you!'
+  
+ 'hello'.toUpperCase() === 'HELLO'
+  return 'YES INDEED!'  
+
+ 
+  
+
+  
+  
+}
